469ffcf72089e0559ed0ed9b75c0928bf4bd5a8e
Diego Rattis <dirattis@gmail.com>
2021-06-02 18:59:50 -0300

    Initial

---
+++ Rattis.Investments.Utilities/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rattis.Investments.Utilities
+{
+    public class Cliente
+    {
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
